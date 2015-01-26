a007010c0013a50c8df6a95e7f9cd9c8eb647be8
Collin Mullis <cmullis@saintjoe.edu>
2015-01-26 12:12:47 -0500

    Added comments to the program

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,6 @@
+//	Collin Mullis' Program. 
+// 	FizzBuzz Program
+//	Program prints out numbers 1 through 100. If the number is divisible by 	3, then it will print out fizz instead, if its divisible by 5, it will 		print out buzz, and if it is divisible by both, then it will print fizzb	uzz, otherwise it will just print the number. 
 for (var number = 1; number <=100; number++) {
 	if (number %3 == 0 && number % 5 == 0)
 		console.log("fizzbuzz");
